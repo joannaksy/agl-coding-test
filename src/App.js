5c30adac4304f83f39e1a8d27c91a30f78259033
Joanna <ksy_1@DESKTOP-BD4QF9I>
2022-05-10 19:49:37 +1000

    --first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+
+  const [catMaleOwnerList, setCatMaleOwnerList] = useState();
+  const [catFemaleOwnerList, setCatFemaleOwnerList] = useState();
+
+  useEffect(() => {
+    const url = 'http://agl-developer-test.azurewebsites.net/people.json';
+    
+    const fetchList = async() => {
+      try {
+        const response = await fetch(url);
+        const json = await response.json();
+        mapCatList(json);
+
+      } catch (error) {
+        console.log("error", error);
+      }
+    };
+
+    const mapCatList = (data) =>{
+      //remove the owner with no pet
+      var catMaleList = [];
+      var catFemaleList = [];
+      data.forEach(element => {
+        if (element.pets != null){
+          if (element.gender === 'Male'){
+            element.pets.forEach(pet =>{
+              if (pet.type === 'Cat')
+                catMaleList.push(pet.name);
+            })
+          }else{
+            element.pets.forEach(pet =>{
+              if (pet.type === 'Cat')
+                catFemaleList.push(pet.name);
+            })
+          }
+        }
+      });
+
+      //Sorting the list and map to list item
+      setCatFemaleOwnerList(mapListintoListItem(catFemaleList));
+      setCatMaleOwnerList(mapListintoListItem(catMaleList));
+
+    }
+
+    const mapListintoListItem=(array)=>{
+      return array.sort().map((owner)=>
+      <li key={owner}> {owner}</li>);
+    }
+
+
+    fetchList();
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Male</h1>
+      {catMaleOwnerList}
+      <h1>Female</h1>
+      {catFemaleOwnerList}
+
+
     </div>
   );
 }
